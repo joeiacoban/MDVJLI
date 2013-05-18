f197d6eb6eda87fbe1f9a18024265e250d50242c
Joseph Iacoban <joeiacoban@fullsail.edu>
2013-05-17 21:28:39 -0400

    Procedure

--- rename-me.js
+++ rename-me.js
@@ -4,3 +4,14 @@
 // Preparing Dinner
 
 // My Variables
+
+// Procedure
+var chefJoeyardine = function(time) {
+	if (time < 7) {
+		console.log("It is only " + time + " so I have time to cook something tasty.");
+	} else {
+		console.log("I will make Ramen Noodles.");
+	}
+};
+chefJoeyardine(6);
+
